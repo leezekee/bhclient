4c687e8b1b3bc841b217e49d8f5f4ae3e5148968
leezekee <841361034@qq.com>
2023-02-09 11:53:01 +0800

    null

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  devServer: {
-    proxy: {
-      '/api': { 
-        target: 'http://39.106.127.16:7001', 
-        // target: 'https://www.vue-js.com/api',
-        changeOrigin: true,
-        pathRewrite: {
-          '^/api': ''
-        }
-      }
-    }
-  },
+  // devServer: {
+  //   proxy: {
+  //     '/api': { 
+  //       target: 'http://39.106.127.16:7001', 
+  //       // target: 'https://www.vue-js.com/api',
+  //       changeOrigin: true,
+  //       pathRewrite: {
+  //         '^/api': ''
+  //       }
+  //     }
+  //   }
+  // },
   // assetsDir: 'static',
   // parallel: false,
   // publicPath: './',
